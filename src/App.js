a90e22255c713a2bbd13a8cb5cc35c73d61cc61b
Thomas Nguyen <ttn@ou.edu>
2022-09-02 20:24:19 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import { useState } from "react";
+import io from "socket.io-client";
+import Chat from "./pages/chat";
+
+const socket = io.connect("http://localhost:4000");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                username={username}
+                setUsername={setUsername}
+                room={room}
+                setRoom={setRoom}
+                socket={socket}
+              />
+            }
+          ></Route>
+          <Route
+            path="/chat"
+            element={<Chat username={username} room={room} socket={socket} />}
+          ></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/chat/index.js
@@ -0,0 +1,20 @@
+import styles from "./styles.module.css";
+import MessagesReceived from "./messages";
+import SendMessage from "./sendmessage";
+import RoomAndUsersColumn from "./room-and-users";
+
+const Chat = ({ socket, username, room }) => {
+  return (
+    <div className={styles.chatContainer}>
+      <div className={styles.chatContainer}>
+        <RoomAndUsersColumn socket={socket} username={username} room={room} />
+      </div>
+      <div>
+        <MessagesReceived socket={socket} />
+        <SendMessage socket={socket} username={username} room={room} />
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
